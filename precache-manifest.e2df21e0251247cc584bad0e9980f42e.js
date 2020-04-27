f7c30922e35b2d107e12adf630b53b4a32b61a9d
babilonsuxx <babilonsuxx@gmail.com>
2020-04-27 11:05:43 +0300

    Updates

---
+++ precache-manifest.e2df21e0251247cc584bad0e9980f42e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ee0dbe86ab2e372a556c0ed226afbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "993e9542115b4286731f",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a19fa3bf40fe723ab79f",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "993e9542115b4286731f",
+    "url": "./static/js/2.a6b8c6b6.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.a6b8c6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19fa3bf40fe723ab79f",
+    "url": "./static/js/main.b9b2c92a.chunk.js"
+  },
+  {
+    "revision": "cb575455054213830690",
+    "url": "./static/js/runtime-main.d1a4fcae.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d6031a674d396a80f0e42e1c942a7cf.js"
+  "./precache-manifest.e2df21e0251247cc584bad0e9980f42e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
